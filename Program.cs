dad8e84473997f90e89f0463248372f95fc83bb1
Nika Kinkladze <65700920+kinkla@users.noreply.github.com>
2023-06-29 15:41:41 +0400

    First Push

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] quotes = ReadQuotesFromFile("C:\\Users\\qinql\\OneDrive\\Desktop\\Random Quote\\Quotes.txt");
+
+        if (quotes.Length == 0)
+        {
+            Console.WriteLine("No quotes found.");
+            return;
+        }
+
+        Random random = new Random();
+        int randomIndex = random.Next(quotes.Length);
+        string randomQuote = quotes[randomIndex];
+
+        Console.WriteLine("Random Quote:");
+        Console.WriteLine(randomQuote);
+    }
+
+    static string[] ReadQuotesFromFile(string filePath)
+    {
+        try
+        {
+            return File.ReadAllLines(filePath);
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine($"File not found: {filePath}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading file: {ex.Message}");
+        }
+
+        return new string[0];
+    }
+}
